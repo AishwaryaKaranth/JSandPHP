c3ec12044e2d51dd71bb77f7e87a261676447e53
AishwaryaKaranth <a.karanth934@gmail.com>
2020-01-16 11:07:08 +0530

    first comment

---
+++ postt.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.php" method="POST"><br>
+    Name:<input type="text" name="name"><br>
+    Email:<input type="email" name="email"><br>
+    Password:<input type="password" name="pass"><br>
+    <!-- Your feedback:<input type="text" name="feed"><br> -->
+    <input type="submit">
+
+
+
+</form>
+</body>
+</html>
